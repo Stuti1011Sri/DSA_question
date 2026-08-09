5a7275b5243f92b285302b45cd99304f952a65b4
Stuti_Srivastava <25113ew027@glbitm.ac.in>
2026-08-09 22:05:09 +0530

    Time: 1 ms (99.98%), Space: 42.3 MB (96.21%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Solution{
+    public int reverse(int n){
+        long rev = 0;
+        while(n!=0){
+            int d = n%10;
+            rev = rev*10+d;
+            n  = n/10;
+            if(rev> Integer.MAX_VALUE || rev< Integer.MIN_VALUE){
+                return 0;
+            }
+        }
+        return (int) rev;
+    }
+    
+}
